bdbeb8cdf4d808f4ac840cee433a97baf836bbf2
Lukáš Salak <lukas.salak1@gmail.com>
2024-02-12 22:39:37 +0100

    ChatGPT4 implementation of Ray

---
+++ Core/Ray.cs
@@ -0,0 +1,18 @@
+﻿namespace Core;
+public class Ray
+{
+    public Vector3 Origin { get; }
+    public Vector3 Direction { get; }
+
+    public Ray(Vector3 origin, Vector3 direction)
+    {
+        Origin = origin;
+        Direction = direction.Normalize(); // Ensuring the direction is normalized
+    }
+
+    // Compute a point along the ray at parameter t
+    public Vector3 At(float t)
+    {
+        return Origin + t * Direction;
+    }
+}
--- Core/Vector3.cs
+++ Core/Vector3.cs
@@ -1,80 +1,77 @@
-﻿namespace Core
+﻿namespace Core;
+
+public class Vector3(float x, float y, float z)
 {
-    using System;
+    public float X { get; set; } = x;
+    public float Y { get; set; } = y;
+    public float Z { get; set; } = z;
 
-    public class Vector3(float x, float y, float z)
+    // Vector addition
+    public static Vector3 operator +(Vector3 a, Vector3 b)
     {
-        public float X { get; set; } = x;
-        public float Y { get; set; } = y;
-        public float Z { get; set; } = z;
+        return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+    }
 
-        // Vector addition
-        public static Vector3 operator +(Vector3 a, Vector3 b)
-        {
-            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
-        }
+    // Vector subtraction
+    public static Vector3 operator -(Vector3 a, Vector3 b)
+    {
+        return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+    }
 
-        // Vector subtraction
-        public static Vector3 operator -(Vector3 a, Vector3 b)
-        {
-            return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
-        }
+    // Vector scaling (multiplication by a scalar)
+    public static Vector3 operator *(Vector3 v, float scalar)
+    {
+        return new Vector3(v.X * scalar, v.Y * scalar, v.Z * scalar);
+    }
 
-        // Vector scaling (multiplication by a scalar)
-        public static Vector3 operator *(Vector3 v, float scalar)
-        {
-            return new Vector3(v.X * scalar, v.Y * scalar, v.Z * scalar);
-        }
+    public static Vector3 operator *(float scalar, Vector3 v)
+    {
+        return v * scalar;
+    }
 
-        public static Vector3 operator *(float scalar, Vector3 v)
-        {
-            return v * scalar;
-        }
+    // Vector division by a scalar
+    public static Vector3 operator /(Vector3 v, float scalar)
+    {
+        return new Vector3(v.X / scalar, v.Y / scalar, v.Z / scalar);
+    }
 
-        // Vector division by a scalar
-        public static Vector3 operator /(Vector3 v, float scalar)
-        {
-            return new Vector3(v.X / scalar, v.Y / scalar, v.Z / scalar);
-        }
+    // Dot product
+    public static float Dot(Vector3 a, Vector3 b)
+    {
+        return a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+    }
 
-        // Dot product
-        public static float Dot(Vector3 a, Vector3 b)
-        {
-            return a.X * b.X + a.Y * b.Y + a.Z * b.Z;
-        }
+    // Cross product
+    public static Vector3 Cross(Vector3 a, Vector3 b)
+    {
+        return new Vector3(
+            a.Y * b.Z - a.Z * b.Y,
+            a.Z * b.X - a.X * b.Z,
+            a.X * b.Y - a.Y * b.X);
+    }
 
-        // Cross product
-        public static Vector3 Cross(Vector3 a, Vector3 b)
-        {
-            return new Vector3(
-                a.Y * b.Z - a.Z * b.Y,
-                a.Z * b.X - a.X * b.Z,
-                a.X * b.Y - a.Y * b.X);
-        }
+    // Normalize the vector
+    public Vector3 Normalize()
+    {
+        float length = (float)Math.Sqrt(X * X + Y * Y + Z * Z);
+        return this / length;
+    }
 
-        // Normalize the vector
-        public Vector3 Normalize()
-        {
-            float length = (float)Math.Sqrt(X * X + Y * Y + Z * Z);
-            return this / length;
-        }
+    // Length of the vector
+    public float Length()
+    {
+        return (float)Math.Sqrt(X * X + Y * Y + Z * Z);
+    }
 
-        // Length of the vector
-        public float Length()
-        {
-            return (float)Math.Sqrt(X * X + Y * Y + Z * Z);
-        }
+    // Squared length of the vector (useful for performance-sensitive code)
+    public float LengthSquared()
+    {
+        return X * X + Y * Y + Z * Z;
+    }
 
-        // Squared length of the vector (useful for performance-sensitive code)
-        public float LengthSquared()
-        {
-            return X * X + Y * Y + Z * Z;
-        }
-
-        // ToString method for easy debugging
-        public override string ToString()
-        {
-            return $"({X}, {Y}, {Z})";
-        }
+    // ToString method for easy debugging
+    public override string ToString()
+    {
+        return $"({X}, {Y}, {Z})";
     }
 }
